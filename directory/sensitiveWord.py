38bc6443704e05595986bd949c4a2e0d815c9550
jjjinlll <liujl-h@glodon.com>
2017-12-08 10:56:54 +0800

    test

---
+++ directory/sensitiveWord.py
@@ -0,0 +1,29 @@
+#/usr/bin/env python
+#coding=utf8
+
+"""
+    后门中包含的特有敏感字符
+    自行手动添加各个类型后门到字典中，格式{"关键字":"类型"}
+"""
+
+#php敏感字符列表
+php_sensitive_words = {
+    "www.phpdp.org":"PHP神盾加密后门",
+    "www.phpjm.net":"PHP加密后门",
+    "phpspy":"phpspy",
+    "cmd.exe":"windows cmd",
+    "WScript.shell":"WScript.shell"
+}
+
+asp_sensitive_words = {
+}
+
+apsx_sensitive_words = {
+}
+
+jsp_sensitive_words = {
+    "java.io.FileOutputStream":"file write",
+    "Runtime.getRuntime().exec":"exec command",
+    "cmd.exe":"windows cmd",
+    "jspspy":"jspspy"
+}
---
+++ directory/webshell.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#coding=utf8
+
+"""
+    文件名包含明显的木马名字和已知webshell名列表
+"""
+
+php_webshell = [
+"phpspy.php",
+"2.php",
+"fuck.php",
+"backdoor.php",
+"shell.php",
+"xxoo.php",
+"1.php"
+]
+
+asp_webshell = [
+]
+
+aspx_webshell = [
+]
+
+jsp_webshell = [
+"1.jsp",
+"2.jsp",
+"fuck.jsp",
+"cmd.jsp",
+"shell.jsp"
+]
